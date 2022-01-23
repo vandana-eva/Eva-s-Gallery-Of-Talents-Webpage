bf165218ecbfc5f4293898fce9e9f05d255acf68
vandana-eva <92171611+vandana-eva@users.noreply.github.com>
2022-01-23 22:04:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function Chess()
+{
+window.location="chess.html";
+}
+function Piano()
+{
+    window.location="piano.html"
+}
+function Origami()
+{
+    window.location="origami.html";
+}
+function Coding()
+{
+    window.location="coding.html";
+}
+function Cook()
+{
+    window.location="baking.html";
+}
+function Art()
+{
+    window.location="art.html";
+} 
+function back()
+{
+    window.location="index.html";
+} 
+function French()
+{
+    window.location="french.html";
+}
+function Calli() 
+{
+    window.location="calli.html";
+}
